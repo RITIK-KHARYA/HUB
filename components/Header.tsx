bf0a784f6bcaeddd0fde4c70e66311efc4ec9297
ritik <ritikkharya05@gmail.com>
2024-05-30 21:18:22 +0530

    basic file addition

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Header() {
+  return (
+    <div className="h-16 bg-gradient-to-t from-[#9945FF] via-[#14F195] to-[#14F195] w-screen">
+      <div></div>
+    </div>
+  );
+}
