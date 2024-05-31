d7d2073d9a661c1eb376e31aee0b1608456ebd2f
ritik <ritikkharya05@gmail.com>
2024-05-31 23:55:00 +0530

    Building the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import { ModeToggle } from "@/components/Mode-toggle";
+import { FlipWords } from "@/components/Opening";
+import { Playfair_Display } from "next/font/google";
+const playfair = Playfair_Display({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="p-2">
-      <ModeToggle />
-    </div>
+    <main className="overflow-x-hidden overflow-y-hidden">
+      <FlipWords
+        className={`justify-center items-center w-[100vw] flex h-44 font-bold text-5xl ${playfair.className}`}
+        words={["Nothing"]}
+      />
+    </main>
   );
 }
